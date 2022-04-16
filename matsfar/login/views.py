f43a881681fa911a568cb8c6c1dc71978f7bcdf2
faridat <bellofaridat1@gmail.com>
2022-04-16 21:33:37 +0100

    update

--- matsfar/login/views.py
+++ matsfar/login/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render 
 from django.http import HttpResponse
+from .models import intel
 
 
 # Create your views here.
 
 def index(request):
-    name = 'ayo'
-    age = 25
-    email = 'ayo@gmail.com'
+    intel1 = intel()
+    intel2 = intel()
+    intel3 = intel()
+    intel4 = intel()
+
+    intel1.id = 1
+    intel1.det = 'Your health is important to us'
+    intel1.st = True
+
+    intel2.id = 2
+    intel2.det = 'Your life is precious to us'
+    intel2.st = True
+
+    intel3.id = 3
+    intel3.det = 'You can never die in our clinic'
+    intel3.st = False
+
+    intel4.id = 4
+    intel4.det = 'Outpatient Facility is Available'
+    intel4.st = False
 
     context = {
-            'name':name,
-            'age':age,
-            'email':email,
-    }
-
+        'intels' : [intel1,intel2,intel3,intel4],
+}
     return render(request,'index.html', context)
+
+
+    
 
 
 
