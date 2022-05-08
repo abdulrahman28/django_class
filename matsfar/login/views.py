8905b78ab94af710561dadc476efe699e2cd1b65
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2022-05-08 01:24:07 +0100

    update

--- matsfar/login/views.py
+++ matsfar/login/views.py
@@ -6,34 +6,19 @@ from .models import intel
 # Create your views here.
 
 def index(request):
-    intel1 = intel()
-    intel2 = intel()
-    intel3 = intel()
-    intel4 = intel()
 
-    intel1.id = 1
-    intel1.det = 'Your health is important to us'
-    intel1.st = True
-
-    intel2.id = 2
-    intel2.det = 'Your life is precious to us'
-    intel2.st = True
-
-    intel3.id = 3
-    intel3.det = 'You can never die in our clinic'
-    intel3.st = False
-
-    intel4.id = 4
-    intel4.det = 'Outpatient Facility is Available'
-    intel4.st = False
+    db = intel.objects.all()
 
     context = {
-        'intels' : [intel1,intel2,intel3,intel4],
+        'intels' : db,
 }
     return render(request,'index.html', context)
 
 
-    
+def signin(request):
+
+
+    return render(request,'signin.html')
 
 
 
