2e42be4cfdbadbc8ab5c2e5815cb3930e64db495
Yusuf Abdulrahman <yusufabdulrahman05@gmail.com>
2022-05-08 01:46:41 +0100

    Update models.py

--- matsfar/login/models.py
+++ matsfar/login/models.py
@@ -7,5 +7,6 @@ class intel(models.Model):
     name = models.CharField(max_length=100)
     det = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
+    id = models.IntegerField(max_length=5,primary_key=True)
 
  
